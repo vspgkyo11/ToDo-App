7a45cfb45e09645d9a16e3998b7cfa16d3ac2224
euronflorida11 <banaichi1@gmail.com>
2025-01-05 23:27:13 +0900

    import Tailwind css

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,13 +3,22 @@
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
-	<title>Welcome to Laravel8 + React.js!</title>
+	<title>Welcome to Laravel8 + React.js + Tailwind CSS!</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<link rel="stylesheet" href="/css/app.css" >
 	<style> .navbar{margin-bottom: 20px;} </style>
 </head>
-<body>
+<body class="bg-gray-100 text-center h-screen flex items-center justify-center">
   <div id="app"></div>
+  <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+	<div className="bg-white p-8 rounded-lg shadow-lg text-center">
+		<h1 className="text-4xl font-bold text-blue-500">Welcome to Tailwind CSS with React</h1>
+		<p className="text-lg mt-4 text-gray-700">Styled using Tailwind CSS</p>
+		<button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+			Get Started
+		</button>
+	</div>
+</div>
   <script src="/js/app.js"></script>
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,5 @@
 const mix = require('laravel-mix');
-
+const tailwindcss = require('tailwindcss');
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -14,4 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .react()
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps(true, 'inline-source-map');
+    .sourceMaps(true, 'inline-source-map')
+    .postCss('resources/css/app.css', 'public/css', [
+        tailwindcss('./tailwind.config.js'),
+    ]);
